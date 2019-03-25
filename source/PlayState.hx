ebee08e0ed4072e90c1df5686508d44ec109033e
Russell604 <rus604@live.missouristate.edu>
2019-03-25 18:47:20 -0500

    created my first hello world in haxeflixel

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,16 @@
 package;
 
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
 	override public function create():Void
 	{
 		super.create();
+		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
+		text.screenCenter();
+		add(text);
 	}
 
 	override public function update(elapsed:Float):Void
