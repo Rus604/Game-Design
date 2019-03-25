a8548c49541afe186f9b952c4dc2fcd23fadffbf
Russell604 <rus604@live.missouristate.edu>
2019-03-25 18:57:22 -0500

    added sprite to hello world frame

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 
@@ -11,6 +12,11 @@ class PlayState extends FlxState
 		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
 		text.screenCenter();
 		add(text);
+		
+		var spr = new FlxSprite(50, 50);
+		spr.velocity.set(25, 55);
+		add(spr);
+		
 	}
 
 	override public function update(elapsed:Float):Void
