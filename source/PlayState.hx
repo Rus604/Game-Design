3e2d36227117beda1721309520cd18b717712224
Russell604 <rus604@live.missouristate.edu>
2019-04-03 14:39:38 -0500

    changed text size

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -9,7 +9,8 @@ class PlayState extends FlxState
 	override public function create():Void
 	{
 		super.create();
-		var text = new flixel.text.FlxText(0, 0, 0, "The Hero", 64);
+		
+		var text = new flixel.text.FlxText(0, 0, 0, "The Hero", 32);
 		text.screenCenter();
 		add(text);
 		
