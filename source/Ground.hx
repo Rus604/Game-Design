9629770a892f11fa5545d075d0bcd2af76e99b26
Russell604 <rus604@live.missouristate.edu>
2019-04-24 16:15:15 -0500

    added ground.hx

--- source/Ground.hx
+++ source/Ground.hx
@@ -9,7 +9,6 @@ import flixel.system.FlxAssets.FlxGraphicAsset;
  */
 class Ground extends FlxSprite 
 {
-
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
