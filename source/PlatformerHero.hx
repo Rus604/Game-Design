35e07188cb88ba7a533a20d2d4fb0af15ec56bf3
Russell604 <rus604@live.missouristate.edu>
2019-04-24 19:13:19 -0500

    platformerHero can jump and fall with gravity

--- source/PlatformerHero.hx
+++ source/PlatformerHero.hx
@@ -17,8 +17,8 @@ class PlatformerHero extends FlxSprite
 		this.makeGraphic(25, 25, FlxColor.WHITE);
 		
 		// Set acceleration and max velocity to represent gravity.
-		this.acceleration.y = 10;
-		this.maxVelocity.y = 50;	
+		this.acceleration.y = 75;
+		this.maxVelocity.y = 200;	
 		
 		// Set max velocity for x to limit speed of character
 		this.maxVelocity.x = 200;	
@@ -28,7 +28,8 @@ class PlatformerHero extends FlxSprite
 	{
 		var leftPressed:Bool = FlxG.keys.pressed.LEFT;
 		var rightPressed:Bool = FlxG.keys.pressed.RIGHT;
-
+		var upPressed:Bool = FlxG.keys.pressed.Z;
+		
 		if (leftPressed)
 		{ 				                // Move left
 			this.acceleration.x = -50;
@@ -36,11 +37,15 @@ class PlatformerHero extends FlxSprite
 		{ 		                        // Move right
 			this.acceleration.x = 50;
 		}
+		else if (upPressed)
+		{
+		this.velocity.y = -50;
+		}
 		else
 		{ 					         	// no action
-			this.velocity.set(0, 0);
-			this.acceleration.set(0, 0);
+			this.velocity.x = 0;
 		}
+		
 		super.update(elapsed);
 	}
 	
